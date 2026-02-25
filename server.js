81eaee31a29494f170549079b354209b54049687
VaibhavShukla2004 <vaibhavshukla1110r@gmail.com>
2026-02-25 17:20:16 +0530

    Feat:Add Middleware

--- server.js
+++ server.js
@@ -12,3 +12,13 @@ app.use('/user',userRoutes);
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+//error handling
+app.use((err,req,res,next) => {
+    console.error(err.stack);
+    res.status(500).json({ message: 'Something went wrong' });
+});
+
+//middleware
+const { logger } = require('./middlewares/logger.middleware');
+app.use(logger);
