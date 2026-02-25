557987b41a8ef99d125e1232703a97a74b23921f
VaibhavShukla2004 <vaibhavshukla1110r@gmail.com>
2026-02-25 17:28:08 +0530

    Fix : Used env vars for PORT

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const app = express();
+require('dotenv').config();
 
-const PORT = 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
