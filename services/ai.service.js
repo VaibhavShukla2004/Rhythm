f6291fbbb91ab611f3a04f23933c96a834d4b6cc
VaibhavShukla2004 <vaibhavshukla1110r@gmail.com>
2026-06-15 13:08:01 +0800

    Gemini test code

---
+++ services/ai.service.js
@@ -0,0 +1,37 @@
+import { GoogleGenAI } from "@google/genai";
+
+//creates gemini client 
+const client = new GoogleGenAI({
+    apiKey: process.env.GEMINI_API_KEY
+});
+
+//generates payload which is the final string to be sent to gemini
+function generatePayload(promptTemplate, lyrics, hint) {
+    const prompt = `
+${promptTemplate}
+
+Hint:
+${hint}
+
+Lyrics:
+${lyrics}
+`;
+
+    return { prompt };
+}
+
+
+//sends payload to AI and gets AI response
+async function getAIResponse(payload) {
+    try {
+        const response = await client.models.generateContent({
+            model: "gemini-3.5-flash",
+            contents: payload.prompt
+        });
+
+        return response.text;
+    } catch (e) {
+        console.error("Gemini API Error:", e);
+        throw e;
+    }
+}
